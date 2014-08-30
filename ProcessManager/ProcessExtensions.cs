77c43e0e02839a0ec29013315ca1d9a9008faf40
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-08-30 11:53:41 +0900

    Add initial files.

---
+++ ProcessManager/ProcessExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Management;
+
+namespace ProcessManager {
+    public static class ProcessExtensions {
+        public static void KillAllProcessesSpawnedBy(this Process process) {
+            KillAllProcessesSpawnedBy((uint)process.Id);
+        }
+
+        public static void KillAllProcessesSpawnedBy(uint parentProcessId) {
+            Debug.WriteLine("Finding processes spawned by process with Id [" + parentProcessId + "]");
+
+            // NOTE: Process Ids are reused!
+            var searcher = new ManagementObjectSearcher(
+                    "SELECT * " +
+                    "FROM Win32_Process " +
+                    "WHERE ParentProcessId=" + parentProcessId);
+            var collection = searcher.Get();
+            if (collection.Count <= 0) {
+                return;
+            }
+            Debug.WriteLine("Killing [" + collection.Count
+                            + "] processes spawned by process with Id [" + parentProcessId + "]");
+            foreach (var item in collection) {
+                var childProcessId = (uint)item["ProcessId"];
+                if ((int)childProcessId == Process.GetCurrentProcess().Id) {
+                    continue;
+                }
+                KillAllProcessesSpawnedBy(childProcessId);
+
+                var childProcess = Process.GetProcessById((int)childProcessId);
+                Debug.WriteLine("Killing child process [" + childProcess.ProcessName + "] with Id ["
+                                + childProcessId + "]");
+                childProcess.Kill();
+            }
+        }
+    }
+}
---
+++ ProcessManager/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using Mono.Options;
+
+namespace ProcessManager {
+    internal class Program {
+        private static void Main(string[] args) {
+            var help = false;
+            var execFilePath = "";
+            var killPid = 0U;
+            var p = new OptionSet {
+                { "s|start=", v => execFilePath = v },
+                { "h|?|help", v => help = v != null }, {
+                    "k|kill=", v => {
+                        if (!uint.TryParse(v, out killPid)) {
+                            Console.Error.WriteLine("The given PID is an invalid value.");
+                            Environment.Exit(-1);
+                        }
+                    }
+                }
+            };
+            var arguments = p.Parse(args);
+
+            if (help) {
+                p.WriteOptionDescriptions(Console.Out);
+            } else if (!string.IsNullOrEmpty(execFilePath)) {
+                using (var proc = new Process {
+                    StartInfo = {
+                        FileName = execFilePath,
+                        Arguments = string.Join(" ", arguments),
+                        UseShellExecute = false,
+                        RedirectStandardOutput = false,
+                        RedirectStandardInput = false,
+                        CreateNoWindow = false,
+                    },
+                }) {
+                    proc.Start();
+                    Console.WriteLine("PID: " + proc.Id);
+                }
+            } else if (killPid != 0) {
+                ProcessExtensions.KillAllProcessesSpawnedBy(killPid);
+            }
+        }
+    }
+}
